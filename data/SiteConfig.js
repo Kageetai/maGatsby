c871db0bab65b05754e24cacd80f439cbba35605
Michael Seifarth <michael@bynder.com>
2017-12-05 16:48:58 +0100

    SiteConfig and moved posts to /posts folder

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,45 +1,45 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogPostDir: "posts", // The name of directory that contains your posts.
   blogPostSort: "date", // which field on the post to sort by
   blogPageSize: 5, // size of pages of blog posts
   dateFormatInput: "DD-MM-YYYY", // date format to use for parsing from source
   dateFormatOutput: "LL", // date format to use for output/rendering, see http://momentjs.com/docs/#/displaying/format/
-  siteTitle: "Gatsby Advanced Starter", // Site title.
-  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
+  siteTitle: "Kageetai.net", // Site title.
+  siteTitleAlt: "maBlog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
+  siteUrl: "https://kageetai.net", // Domain of your website without pathPrefix.
   pathPrefix: "/", // (Folder-)Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/. Only set when actually deploying to a subfolder or so, because otherwise config breaks.
-  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "A blog for my personal projects.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
-  userName: "Advanced User", // Username to display in the author segment.
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  siteFBAppID: "152804634760737", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-12320225-1", // GA tracking ID.
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "generic", // Default category for posts.
+  userName: "Kageetai", // Username to display in the author segment.
+  userTwitter: "kageetai", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Barcelona, Spain", // User location to display in the author segment.
+  userAvatar: "https://s.gravatar.com/avatar/c2444d064665b19ed31e3e69570e6581?s=80", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-advanced-starter",
+      url: "https://github.com/kageetai",
       iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
+      url: "https://twitter.com/kageetai",
       iconClassName: "fa fa-twitter"
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:ma@kageetai.net",
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2017. Kageetai.net", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
