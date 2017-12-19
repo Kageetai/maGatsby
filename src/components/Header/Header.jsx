0ca947b743f448fb96115eb4d70acee6b82afbe9
Michael Seifarth <michael@bynder.com>
2017-12-19 18:33:57 +0100

    calculate header background position based on scroll event and header height

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,11 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photos: []
+      photos: [],
+      randomPhoto: "",
+      headerBackgroundPositionY: 50
     };
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
@@ -17,33 +20,53 @@ class Header extends Component {
         this.props.config.flickrApiKey
       }&user_id=${
         this.props.config.flickrUser
-      }&format=json&nojsoncallback=1&extras=url_l`
+      }&format=json&nojsoncallback=1&extras=url_o`
     )
       .then(response => response.json())
       .then(response =>
         this.setState({
-          photos: response.photos.photo
+          photos: response.photos.photo,
+          randomPhoto:
+            this.state.photos && this.state.photos.length
+              ? Math.floor(Math.random() * this.state.photos.length)
+              : 0
         })
       )
       // eslint-disable-next-line no-console
       .catch(error => console.error(error));
+
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    this.setState({
+      headerBackgroundPositionY:
+        50 / -this.headerElement.clientHeight * window.scrollY + 50
+    });
   }
 
   render() {
     const { config } = this.props;
-    const randomPicture =
-      this.state.photos && this.state.photos.length
-        ? Math.floor(Math.random() * this.state.photos.length)
-        : 0;
 
     const headerStyle = {
       backgroundImage: this.state.photos.length
-        ? `url(${this.state.photos[randomPicture].url_l})`
-        : defaultHeaderBackground
+        ? `url(${this.state.photos[this.state.randomPhoto].url_o})`
+        : defaultHeaderBackground,
+      backgroundPositionY: `${this.state.headerBackgroundPositionY}%`
     };
 
     return (
-      <header className="header" style={headerStyle}>
+      <header
+        className="header"
+        style={headerStyle}
+        ref={element => {
+          this.headerElement = element;
+        }}
+      >
         <Link to="/">
           <h1>{config.siteTitle}</h1>
         </Link>
